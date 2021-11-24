0bd16edd6de61f8007cf442bafeadd150408b0ce
Pierre Inza <pierreinza@hotmail.fr>
2021-11-24 14:51:17 +0100

    commit buttons et focus work

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,6 @@ function scrollCheck() {
                 passer.style.display = "block";
             } else if (document.documentElement.scrollTop == 0) {
                 passer.style.display = "none";
-
             }
 
         }
@@ -29,7 +28,7 @@ aPass.addEventListener('click', function() {
 
 //Ici nous allons implementer une variable qui va gerer si nous sommes en Local ou sur GitHub. 
 // Cette variable doit etre egale à "/Projet06_FishEye" si nous sommes sur GitHub, sinon en Local ce sera "." un champVide.
-let linkHelperLocalVsGitHub = "/Projet06_FishEye"
+let linkHelperLocalVsGitHub = "."
 
 const data = async() => {
 
@@ -41,14 +40,14 @@ data().then(v => showProducts(v))
     //---------J'AFFICHE TOUS LES PRODUITS---------
 
 function showProducts(data) {
-    for (product of data) {
+    for (let product of data) {
         for (let i = 0; i < product.photographers.length; i++) {
             var article = document.createElement("ARTICLE");
             var articleContent = `<a href = "${linkHelperLocalVsGitHub}/photographe.html?id=${product["photographers"][i].id}"><div id ="${product["photographers"][i].portrait}"></div><h2>${product["photographers"][i].name}</h2><h3>${product["photographers"][i].city}</h3><h4>${product["photographers"][i].tagline}</h4><p>${product["photographers"][i].price}€/jour</p><ul></ul></a>`
             article.innerHTML = articleContent;
             let ul = article.getElementsByTagName("UL")[0];
             //Je vais creer les Tags pour mes Photographes.
-            for (tag of product["photographers"][i].tags) {
+            for (let tag of product["photographers"][i].tags) {
                 let li = document.createElement("LI");
 
                 li.innerHTML = `#${tag}`;
@@ -57,7 +56,7 @@ function showProducts(data) {
             section.appendChild(article);
             var _photoDiv = document.getElementById(`${product["photographers"][i].portrait}`);
             _photoDiv.style.backgroundImage = `url("${linkHelperLocalVsGitHub}/assets/Sample_Photos/profils/${product["photographers"][i].portrait}")`;
-            let Ul = article.getElementsByTagName("UL");
+
         }
         // _photoDiv.style.backgroundImage = "../assets/Sample_Photos/profils/Mimi.jpg";
         //   article.innerHTML = articleContent;
@@ -69,20 +68,27 @@ function showProducts(data) {
 
 }
 //#################ICI on recupere les li de la nav pour manager la navigation#####################
-let LiDeLaNavPrincipale = document.getElementsByTagName("header")[0].getElementsByTagName("nav")[0].getElementsByTagName("UL")[0].getElementsByTagName("LI");
+let buttonDeLaNavPrincipale = document.getElementsByTagName("header")[0].getElementsByTagName("nav")[0].getElementsByTagName("UL")[0].getElementsByTagName("BUTTON");
 let listFiltres = [];
 
 
-for (li of LiDeLaNavPrincipale) {
-    let content = li.innerHTML.substr(1).toLowerCase();
-    li.addEventListener('click', function() {
+for (let button of buttonDeLaNavPrincipale) {
+    let content = button.innerHTML.substr(1).toLowerCase();
+    button.addEventListener('click', function() {
 
-        /*Cette commande permet de filtrer seulement pour un seul TAg */
-        if (listFiltres[0] == content) {
+        //Cette fonction va etre appelé quand on voudra afficher tous les photographes. 
+        function displayAllVignettes() {
             let lesArticles = document.getElementsByTagName("ARTICLE");
-            for (article of lesArticles) {
+            for (let article of lesArticles) {
                 article.style.display = "block";
             }
+        }
+
+
+
+        /*Cette commande permet de filtrer seulement pour un seul TAg à la fois*/
+        if (listFiltres[0] == content) {
+            displayAllVignettes();
             listFiltres = [];
         } else {
             listFiltres.length >= 1 ? listFiltres = [] : null;
@@ -95,7 +101,8 @@ for (li of LiDeLaNavPrincipale) {
 
 function manageList(filtre) {
     let supprimer = false;
-    for (presentFiltre of listFiltres) {
+    //Ceci n'a pas de sens s'il y a seulement tri un filtre aprés l'autre, mais pourrat prendre son sens si le client a besoin de plusieurs. filtres à la fois. 
+    for (let presentFiltre of listFiltres) {
         if (filtre == presentFiltre) {
             supprimer = true;
         }
@@ -104,30 +111,26 @@ function manageList(filtre) {
     if (supprimer == true) {
         let _index = listFiltres.indexOf(filtre);
         listFiltres.splice(_index, 1);
-        /*_________SUpprime l'element par index________*/
+        /*_________Supprime l'element par index________*/
     } else { listFiltres.push(filtre); }
     if (listFiltres.length != 0) {
         displayPhotographes();
     } else {
         /*Vas afficher toutes les vignettes de photographe quand les filtres sont à Zero */
-        let articles = document.getElementsByTagName("ARTICLE");
-        for (article of articles) {
-            article.style.display = 'block';
-        }
-
+        this.displayAllVignettes();
     }
 }
 
 /*Cette fonction va regarder si les filtres correspondent ou pas et vas s'occuper de masquer ou non les elements articles.*/
 function displayPhotographes() {
     let listArticles = document.getElementsByTagName("ARTICLE");
-    for (article of listArticles) {
+    for (let article of listArticles) {
         let display = false;
 
         /* POur chaque article */
         let lisArticle = article.getElementsByTagName('LI');
-        for (li of lisArticle) {
-            for (filtre of listFiltres) {
+        for (let li of lisArticle) {
+            for (let filtre of listFiltres) {
                 if (filtre == li.innerHTML.substr(1)) {
                     display = true;
                 }
@@ -146,15 +149,16 @@ function displayPhotographes() {
 }
 
 function manageNavBar() {
+    let buttonDeLaNavPrincipale = document.getElementsByTagName("header")[0].getElementsByTagName("nav")[0].getElementsByTagName("UL")[0].getElementsByTagName("BUTTON");
     if (listFiltres.length >= 1) {
-        for (li of LiDeLaNavPrincipale) {
+        for (let li of buttonDeLaNavPrincipale) {
+            console.log(buttonDeLaNavPrincipale.length);
             let content = li.innerHTML.substr(1).toLowerCase();
             content == listFiltres[0] ? li.className = "clicked" :
                 li.className = "";
         }
     } else {
-
-        for (li of LiDeLaNavPrincipale) {
+        for (let li of buttonDeLaNavPrincipale) {
             li.className = ""
         }
     }
--- JS/photographes.js
+++ JS/photographes.js
@@ -1,11 +1,21 @@
 let idPhotographe = new URL(window.location.href).searchParams.get('id');
 let currentPhotographe;
 let section = document.getElementsByTagName("SECTION")[0];
+let oppenForm = document.getElementById('btn-ContactezMoi');
+let closeFormLogo = document.getElementById('closeForm');
+let btnEnvoyer = document.getElementById('envoyer');
+let listDisplayed = document.getElementById("navigator");
+let corpPrincipale = document.getElementById('corpsPrincipale');
+let lightBox = document.getElementById("lightBox");
+let close = document.getElementById('closeLogo');
+let tarifJournalier = document.getElementById('tarifJournalier')
+let compteur_likes_total_displayed = document.getElementById('compteur_likes_total_displayed');
 let listMedia = [];
+let totalLikes = 0;
 
 //Ici nous allons implementer une variable qui va gerer si nous sommes en Local ou sur GitHub. 
 // Cette variable doit etre egale à "/Projet06_FishEye" si nous sommes sur GitHub, sinon en Local ce sera "." .
-let linkHelperLocalVsGitHub = "/Projet06_FishEye"
+let linkHelperLocalVsGitHub = "."
 
 
 const data = async() => {
@@ -14,12 +24,12 @@ const data = async() => {
     return await fetch('https://raw.githubusercontent.com/Pierrote52/Projet06_FishEye/master/JSON.json').then((v) => { return v.json() });
 }
 
-https: //raw.githubusercontent.com/Pierrote52/Projet06_FishEye/master/JSON.json
-    data().then(v => takeCurrentPhotographe(v));
+//https: //raw.githubusercontent.com/Pierrote52/Projet06_FishEye/master/JSON.json
+data().then(v => takeCurrentPhotographe(v));
 
 
 function takeCurrentPhotographe(data) {
-    for (photographe of data.photographers) {
+    for (let photographe of data.photographers) {
         if (photographe.id == idPhotographe) {
             currentPhotographe = photographe;
             displayPhotographeInfo(photographe);
@@ -36,13 +46,14 @@ function displayPhotographeInfo(photographe) {
     let slogan = vignettePhotographe.getElementsByTagName("H3")[0];
     let photoProfil = vignettePhotographe.getElementsByTagName("DIV")[0];
     let ulFiltres = vignettePhotographe.getElementsByTagName("UL")[0];
-    //Assigner ls variables.
+    //Assigner les variables.
     name.innerHTML = photographe.name;
     localistion.innerHTML = `${photographe.city}, ${photographe.country}`;
     slogan.innerHTML = photographe.tagline;
+    tarifJournalier.innerHTML = `${photographe.price}€/jour`;
     photoProfil.style.backgroundImage = `url('${linkHelperLocalVsGitHub}/assets/Sample_Photos/profils/${photographe.portrait}')`;
-    for (tag of photographe.tags) {
-        li = document.createElement("LI");
+    for (let tag of photographe.tags) {
+        let li = document.createElement("LI");
         li.innerHTML = `<p>#${tag}<p>`;
         ulFiltres.appendChild(li);
     }
@@ -51,16 +62,17 @@ function displayPhotographeInfo(photographe) {
 //Cette fonction va recuperer les photos présentes dans la galerie pour les afficher. 
 function filtrePhotos(data) {
 
-    for (media of data.media) {
+    for (let media of data.media) {
         if (media.photographerId == idPhotographe) {
             listMedia.push(media);
         }
     }
+    displayNavigation();
     createVignette(listMedia, data.photographers);
 }
 //Creer une vignette. 
 function createVignette(Listmedia) {
-    for (media of Listmedia) {
+    for (let media of Listmedia) {
         if (media.image != null) {
             createPhoto(media);
         } else {
@@ -77,14 +89,15 @@ function getUrlMedia(media) {
 }
 
 function createVideo(media) {
+    stateLikesTotal(media.likes);
     let urlVideo = getUrlMedia(media.video);
     let article = document.createElement("ARTICLE");
-    article.innerHTML = `<div class= "image"><video src= "${urlVideo}" ></video></div>` +
+    article.innerHTML = `<a href= '#'><div class= "image"><video src= "${urlVideo}" ></video></div></a>` +
         "<div class = \"TitreEtLikes\">" +
         "<p>" +
         media.title +
         "</p>" +
-        "<div><p>" + media.likes + `</p><img src='${linkHelperLocalVsGitHub}/assets/logos/heart-solid.svg' width='20'></div>` +
+        "<div><p>" + media.likes + `</p><button type="button"><img src='${linkHelperLocalVsGitHub}/assets/logos/heart-solid.svg' width='20'></button></div>` +
         "</div>";
     //Recupere la div des likes et logo heart. 
     let counterEtLikes = article.getElementsByTagName("DIV")[2];
@@ -94,34 +107,39 @@ function createVideo(media) {
         let newInt = parseInt(newP.innerHTML) + 1;
         newP.innerHTML = newInt;
     })
-    let video = article.getElementsByTagName("video")[0];
-    video.addEventListener('click', function() {
+    let video = article.getElementsByTagName("a")[0];
+    video.addEventListener('click', (e) => {
+        e.preventDefault
         displayLighBoxByIndex(listMedia.indexOf(media));
     })
 
 
     section.appendChild(article);
 }
-
+//Crée une photo avec un objet media.
 function createPhoto(media) {
+    stateLikesTotal(media.likes)
+
     let article = document.createElement("ARTICLE");
-    article.innerHTML = "<div class=\"image\"></div>" +
+    article.innerHTML = "<a href=\"#\"><div class=\"image\"></div></a>" +
         "<div class = \"TitreEtLikes\">" +
         "<p>" +
         media.title +
         "</p>" +
-        "<div><p>" + media.likes + `</p><img src='${linkHelperLocalVsGitHub}/assets/logos/heart-solid.svg' width='20'></div>` +
+        "<div><p class='likes'>" + media.likes + `</p><button type="button"><img src='${linkHelperLocalVsGitHub}/assets/logos/heart-solid.svg' width='20'></button></div>` +
         "</div>";
     //Recupere la div des likes et logo heart. 
     let counterEtLikes = article.getElementsByTagName("DIV")[2];
-    //S'Occupe de l'incrémentation des likes sur la page. 
+    //S'occupe de l'incrémentation des likes sur la page, et d'envoye l'information au state générale. 
     counterEtLikes.addEventListener("click", function() {
         let newP = counterEtLikes.getElementsByTagName("P")[0];
         let newInt = parseInt(newP.innerHTML) + 1;
+        stateLikesTotal(1);
         newP.innerHTML = newInt;
     })
-    let img = article.getElementsByTagName("DIV")[0];
-    img.addEventListener("click", function() {
+    let img = article.getElementsByTagName("a")[0];
+    img.addEventListener("click", (e) => {
+        e.preventDefault;
         displayLighBoxByIndex(listMedia.indexOf(media));
     })
     let url = getUrlMedia(media.image);
@@ -129,12 +147,33 @@ function createPhoto(media) {
     divMedia.style.backgroundImage = `url(${url})`;
     section.appendChild(article);
 }
-//Display la lignBox par index.
+//Display la lightBox par index.
 function displayLighBoxByIndex(indexOfMedia) {
-    //Affiche la LighBox
-    let corpPrincipale = document.getElementById('corpsPrincipale');
+    window.addEventListener('keyup', (event) => {
+            switch (event.key) {
+                case 'ArrowLeft':
+                    if (indexOfMedia > 0) {
+                        indexOfMedia -= 1;
+                        displayLightBoxMedia(listMedia[indexOfMedia]);
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (indexOfMedia <= listMedia.length - 2) {
+                        indexOfMedia += 1;
+                        displayLightBoxMedia(listMedia[indexOfMedia]);
+                    }
+                    break;
+                case 'Escape':
+                    corpPrincipale.style.display = "block";
+                    lightBox.style.display = "none";
+                    window.removeEventListener('keyup', true);
+            }
+
+
+        })
+        //Affiche la LighBox
     corpPrincipale.style.display = "none";
-    let lightBox = document.getElementById("lightBox");
+
     lightBox.style.display = "block";
     let media;
 
@@ -153,13 +192,21 @@ function displayLighBoxByIndex(indexOfMedia) {
     }
     //Gere le click sur le chevron back. 
     let back = document.getElementById("back");
-
+    //Navigation quand on veut aller au media précédent. 
     back.addEventListener('click', function() {
         if (indexOfMedia > 0) {
             indexOfMedia -= 1;
             displayLightBoxMedia(listMedia[indexOfMedia]);
         }
     });
+    back.addEventListener('keyup', function() {
+            if (indexOfMedia > 0) {
+                indexOfMedia -= 1;
+                displayLightBoxMedia(listMedia[indexOfMedia]);
+            }
+
+        })
+        //Navigation quand on veut aller au media suivant. 
     let next = document.getElementById("next");
     next.addEventListener('click', function() {
         if (indexOfMedia <= listMedia.length - 2) {
@@ -168,7 +215,9 @@ function displayLighBoxByIndex(indexOfMedia) {
         }
     });
 
+
     function displayLightBoxMedia(nouveauMedia) {
+
         if (nouveauMedia.image != null) {
             let url = getUrlMedia(listMedia[indexOfMedia].image);
             media.innerHTML = "";
@@ -182,49 +231,113 @@ function displayLighBoxByIndex(indexOfMedia) {
 
     }
 
-    let close = document.getElementById('closeLogo');
+
     close.addEventListener('click', function() {
-        //Souci cela creer trop d'elements. A refaire. 
-
-        let corpPrincipale = document.getElementById('corpsPrincipale');
         corpPrincipale.style.display = "block";
-        let lightBox = document.getElementById("lightBox");
         lightBox.style.display = "none";
-
     })
 
 }
-
 //Ouvre le formulaire. 
-let oppenForm = document.getElementById('btn-ContactezMoi');
 oppenForm.addEventListener('click', function() {
     displayFormContact();
 });
 
+
+//Cette fonction affiche le formulaire de Contact.
 function displayFormContact() {
     let pagePhotos = document.getElementById('corpsPrincipale');
+    pagePhotos.ariaHidden = true;
     pagePhotos.style.opacity = "0.3";
     let form = document.getElementById('formulaire');
+    form.ariaHidden = false;
+    let premierInput = document.getElementById('fprenom');
     form.style.display = "block";
+    window.addEventListener('keyup', (event) => {
+        if (event.key == 'Escape') {
+            closeForm();
+        }
+    });
+
+    premierInput.focus();
+
 }
 //Ferme le formulaire. 
-let closeForm = document.getElementById('closeForm');
-closeForm.addEventListener('click', function() {
-    let pagePhotos = document.getElementById('corpsPrincipale');
-    pagePhotos.style.opacity = "1";
-    let form = document.getElementById('formulaire');
-    form.style.display = "none";
+closeFormLogo.addEventListener('click', function() {
+    window.removeEventListener('keyup', true);
+    closeForm();
+
 });
 
+function closeForm() {
+    let pagePhotos = document.getElementById('corpsPrincipale');
+    pagePhotos.ariaHidden = false;
+    pagePhotos.style.opacity = "1";
+    let form = document.getElementById('formulaire');
+    form.ariaHidden = true;
+    form.style.display = "none";
+}
 //Envoie des infos dans le formulaire. 
-let btnEnvoyer = document.getElementById('envoyer');
 btnEnvoyer.addEventListener('click', function(event) {
-    let champs = [];
-    event.preventDefault();
-    let prenom = champs.push(document.getElementById('fprenom').value);
-    let nom = champs.push(document.getElementById('lnom').value);
-    let email = champs.push(document.getElementById('lemail').value);
-    let message = champs.push(document.getElementById('lmessage'));
-    console.log(champs);
+        let champs = [];
+        event.preventDefault();
+        champs.push(document.getElementById('fprenom').value);
+        champs.push(document.getElementById('lnom').value);
+        champs.push(document.getElementById('lemail').value);
+        champs.push(document.getElementById('lmessage').value);
+        console.log(champs);
 
-})
+    })
+    //Gere les likes de compteur Likes Total. 
+function stateLikesTotal(likes) {
+    totalLikes += likes;
+    compteur_likes_total_displayed.innerHTML = totalLikes;
+
+}
+
+
+
+//---Cette partie s'occupe du tri dans les Trier par------
+
+function displayNavigation() {
+    let buttonListDisplayed = listDisplayed.getElementsByTagName('button');
+    let cathegorieTri = ["Popularité", "Date", "Titre"];
+    for (let i = 0; i < 3; i++) {
+        buttonListDisplayed[i].innerHTML = cathegorieTri[i];
+        buttonListDisplayed[i].addEventListener('click', function() {
+            totalLikes = 0;
+            if (this.innerHTML == "Popularité") {
+                trierMediaParPopularite();
+            } else if (this.innerHTML == "Date") {
+                trierMediaParDate();
+            } else if (this.innerHTML == "Titre") {
+                trierMediaParTitre();
+            }
+            section.innerHTML = "";
+            createVignette(listMedia);
+        })
+    }
+}
+
+function trierMediaParPopularite() {
+    listMedia.sort(function(a, b) {
+        return b.likes - a.likes;
+    });
+}
+
+function trierMediaParTitre() {
+    function SortArray(x, y) {
+        return x.title.localeCompare(y.title);
+    }
+    listMedia.sort(SortArray);
+}
+
+function trierMediaParDate() {
+    function SortArray(x, y) {
+        return x.date.localeCompare(y.date);
+    }
+    listMedia.sort(SortArray);
+
+
+
+}
